cbf516295d757b4d95ae91346412cfbe615367af
santiago gonzalez <leosag2507@gmail.com>
2025-03-30 20:46:49 -0300

    add division dataset test and training

--- prototipo.py
+++ prototipo.py
@@ -6,6 +6,7 @@ import pandas as pd
 ##import matplotlib.pyplot as plt  
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression  
+from sklearn.model_selection import train_test_split
 
 # Leo el dataset desde el archivo CSV
 filepath = r"./DataSet_desempenio.csv"
@@ -19,6 +20,10 @@ X = df.drop(['Nombre', 'Apellido', 'Puntaje_desempeño'], axis=1)
 #y representa el objetivo => Puntaje_desempeño
 y = df["Puntaje_desempeño"] 
 
+#dividimos el dataset en un conjunto de entrenamiento y uno de prueba
+#test_size=0.4 40% de data es para test y 60% para training
+#random_state=1 la division es constante, la misma division aleatoria cada vez
+x_train, x_test, y_train, x_test = train_test_split(X, y, test_size = 0.4, random_state = 1)
 
 #Graficamos los datos cuando nos lo da el modelo
 # plt.scatter(x, y, color='blue')
