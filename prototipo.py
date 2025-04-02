0f5a6f182d1dfb5a6b395d30f67a1039598d7d5f
santiago gonzalez <leosag2507@gmail.com>
2025-04-02 19:23:50 -0300

    fix

--- prototipo.py
+++ prototipo.py
@@ -32,11 +32,10 @@ model = LinearRegression()
 #Entrenamos el modelo con los datos de entrenamiento
 model.fit(x_train, y_train)
 
-#Momentaniamente muestro los coeficientes y el incercepto
+#Momentaniamente muestro los coeficientes y el intercepto
 print('Coeficientes:', model.coef_)
 print('Intercepcion', model.intercept_)
 
-<<<<<<< HEAD
 # Realizamos predicciones con el conjunto de prueba
 y_pred = model.predict(x_test)
 
@@ -49,20 +48,9 @@ mse_train = mean_squared_error(y_train, y_train_pred)
 print(f'MSE en conjunto de entrenamiento: {mse_train}')
 print(f'MSE en conjunto de prueba: {mse}')
 
-
-=======
 #Hacemos una prediccion del modelo de regresion lineal
 pred = model.predict(x_test)
 
 #Calculamos el error cuadratico medio(MSE) usando la prediccion
 mse = mean_squared_error(y_test,pred)
 print('MSE:',mse)
->>>>>>> beeee31c9f14cdb336c9be3971b5f5451fa67ec7
-
-#Graficamos los datos cuando nos lo da el modelo
-# plt.scatter(x, y, color='blue')
-# plt.xlabel('Puntaje de desempeño real')
-# plt.ylabel('Puntaje de desempeño predichos')
-# plt.title('Comparacion entre valores reales y predichos')
-# plt.grid(True)
-# plt.show()
