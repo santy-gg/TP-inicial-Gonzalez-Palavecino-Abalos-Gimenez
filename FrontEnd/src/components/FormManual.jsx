629d671d43098eba402cbe6e057218be74b0112c
santiago gonzalez <leosag2507@gmail.com>
2025-04-09 02:43:55 -0300

    update formManual

--- FrontEnd/src/components/FormManual.jsx
+++ FrontEnd/src/components/FormManual.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./FormManual.module.css";
 
 function FormManual() {
+  const [horas_capacitacion, setHoras_capacitacion] = useState("");
+  const [antiguedad, setAntiguedad] = useState("");
+  const [calificacion_previa, setCalificacion_previa] = useState("");
+  const [predictionResult, setPredictionResult] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      Horas_capacitacion: horas_capacitacion,
+      Antiguedad: antiguedad,
+      Calificacion_previa: calificacion_previa,
+    };
+
+    try {
+      const response = await fetch(
+        "http://127.0.0.1:5000/ingresar_manualmente",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data), // Envía los datos como JSON
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error en la API: ${response.statusText}`);
+      }
+
+      const result = await response.json(); // Parsear la respuesta como JSON
+      setPredictionResult(result.prediccion_manual); // Usa el valor de la predicción en la respuesta
+      console.log(predictionResult);
+    } catch (err) {
+      console.error("Error llamando a la API:", err);
+      setError(
+        "Hubo un problema al obtener la predicción. Intenta nuevamente."
+      );
+    }
+  };
+
   return (
     <>
       <div className={styles.form_container}>
@@ -13,7 +55,7 @@ function FormManual() {
           </p>
         </div>
 
-        <form className={styles.formulario_manual}>
+        <form className={styles.formulario_manual} onSubmit={handleSubmit}>
           <div className={styles.group_box}>
             <label className={styles.etiqueta_form}>
               Horas de Capacitación
@@ -22,6 +64,8 @@ function FormManual() {
               type="number"
               className={styles.input_manual}
               placeholder="0"
+              value={horas_capacitacion}
+              onChange={(e) => setHoras_capacitacion(e.target.value)}
             ></input>
           </div>
           <div className={styles.group_box}>
@@ -30,6 +74,8 @@ function FormManual() {
               type="number"
               className={styles.input_manual}
               placeholder="0"
+              value={antiguedad}
+              onChange={(e) => setAntiguedad(e.target.value)}
             ></input>
           </div>
           <div className={styles.group_box}>
@@ -38,20 +84,19 @@ function FormManual() {
               type="number"
               className={styles.input_manual}
               placeholder="0.0"
+              value={calificacion_previa}
+              onChange={(e) => setCalificacion_previa(e.target.value)}
             ></input>
           </div>
           <div className={styles.buttons_manual}>
             <button className={styles.form_button} type="submit">
               Predecir
             </button>
-            <button className={styles.form_button}>Cancelar</button>
           </div>
         </form>
         <div className={styles.prediccion_manual}>
           <h2>Resultado de la prediccion:</h2>
-          <p className={styles.resultado_manual}>
-            prediccion (hay que integrar el script)
-          </p>
+          <p className={styles.resultado_manual}>{predictionResult}</p>
         </div>
       </div>
     </>
